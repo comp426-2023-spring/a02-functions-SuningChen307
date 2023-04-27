0d1a0e8a35d4c79588c3c870b13bdb72e3397116
SuningChen <tuning@live.unc.edu>
2023-04-26 20:45:55 -0400

    help text and time zone

--- cli.js
+++ cli.js
@@ -1,3 +1,24 @@
-#!/usr/bin/env nod
+#!/usr/bin/env node
 
-e
+import minimist from 'minimist';
+import fetch from 'node-fetch';
+import moment from 'moment-timezone';
+ 
+//the help text
+
+var args = minimist(process.argv.slice(2));
+
+if (args.h) {
+	console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit`);
+	process.exit(0);
+}
+
+//time zone
+const timezone = moment.tz.guess();
+
